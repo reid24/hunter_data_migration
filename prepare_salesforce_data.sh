26aca951e420eb1c4e76781780b0361254d88230
Reid Beckett <reid.beckett@cloudwareconnections.com>
2021-05-20 15:49:51 -0400

    uncomment comments

--- prepare_salesforce_data.sh
+++ prepare_salesforce_data.sh
@@ -12,54 +12,48 @@ echo ""
 echo "*** Setup ****"
 mysql -u root hunter_sfdc < setup.sql
 
-# May have to do this command in mysql if this creates an error:
-# SHOW VARIABLES LIKE 'local_infile';
-# SET GLOBAL local_infile = 1;
+echo ""
+echo "*** Program ****"
+mysql -u root hunter_sfdc < programs.sql
 
-# mysql --local-infile -u root hunter_sfdc < setup.sql
+echo ""
+echo "*** Account (ERP) ****"
+echo "Parents..."
+mysql -u root hunter_sfdc < accounts_parents.sql
+echo "Children..."
+mysql -u root hunter_sfdc < accounts_children.sql
 
-# echo ""
-# echo "*** Program ****"
-# mysql -u root hunter_sfdc < programs.sql
+echo ""
+echo "*** Contact ****"
+mysql -u root hunter_sfdc < contacts.sql
 
-# echo ""
-# echo "*** Account (ERP) ****"
-# echo "Parents..."
-# mysql -u root hunter_sfdc < accounts_parents.sql
-# echo "Children..."
-# mysql -u root hunter_sfdc < accounts_children.sql
+echo ""
+echo "*** Lead ****"
+mysql -u root hunter_sfdc < leads.sql
 
-# echo ""
-# echo "*** Contact ****"
-# mysql -u root hunter_sfdc < contacts.sql
+echo ""
+echo "*** Opportunity ****"
+mysql -u root hunter_sfdc < opportunities.sql
 
-# echo ""
-# echo "*** Lead ****"
-# mysql -u root hunter_sfdc < leads.sql
+echo ""
+echo "*** Case ****"
+mysql -u root hunter_sfdc < cases.sql
 
-# echo ""
-# echo "*** Opportunity ****"
-# mysql -u root hunter_sfdc < opportunities.sql
+echo ""
+echo "*** Number Lookup Contacts ****"
+mysql -u root hunter_sfdc < number_lookup_contacts.sql
 
-# echo ""
-# echo "*** Case ****"
-# mysql -u root hunter_sfdc < cases.sql
+echo ""
+echo "*** Distributor Health Checks ****"
+mysql -u root hunter_sfdc < distributor_health_checks.sql
 
-# echo ""
-# echo "*** Number Lookup Contacts ****"
-# mysql -u root hunter_sfdc < number_lookup_contacts.sql
+echo ""
+echo "*** Sales Summary Data ****"
+mysql -u root hunter_sfdc < sales_summary_data.sql
 
-# echo ""
-# echo "*** Distributor Health Checks ****"
-# mysql -u root hunter_sfdc < distributor_health_checks.sql
-
-# echo ""
-# echo "*** Sales Summary Data ****"
-# mysql -u root hunter_sfdc < sales_summary_data.sql
-
-# echo ""
-# echo "*** FSM Sites ***"
-# mysql -u root hunter_sfdc < fsm_sites.sql
+echo ""
+echo "*** FSM Sites ***"
+mysql -u root hunter_sfdc < fsm_sites.sql
 
 echo ""
 echo "*** Meetings ***"
