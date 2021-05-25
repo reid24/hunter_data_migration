811964c3067ca55ca53b10009e6c371c91c92afe
Reid Beckett <rbeckett@a5corp.com>
2021-05-25 08:39:46 -0400

    added audit fields

--- cases.sql
+++ cases.sql
@@ -11,7 +11,10 @@ CREATE TABLE mig_case (
     Desired_Visit_By__c varchar(255),
     Duration_Hours__c varchar(255),
     Description text,
-    Status varchar(255)
+    Status varchar(255),
+	OwnerId char(36) DEFAULT NULL,
+	CreatedById char(36) DEFAULT NULL,
+	CreatedDate datetime DEFAULT NULL
 );
 
 INSERT INTO mig_case (
@@ -24,7 +27,10 @@ INSERT INTO mig_case (
     Desired_Visit_By__c,
     Duration_Hours__c,
     Description,
-    Status
+    Status,
+    OwnerId, 
+    CreatedById, 
+    CreatedDate
 ) (
     SELECT 
     fst.id,
@@ -36,18 +42,22 @@ INSERT INTO mig_case (
     fstc.desired_time_frame_for_visit_c,
     fstc.duration_in_hours_c,
     fstc.problem_description_c,
-    fstc.request_status_c
+    fstc.request_status_c,
+    owner_user.id,
+    creator.id,
+    fst.date_entered
     FROM 
-    fst_field_service_tickets fst
-    INNER JOIN fst_field_service_tickets_cstm fstc ON fstc.id_c = fst.id
+    hunter.fst_field_service_tickets fst
+    INNER JOIN hunter.fst_field_service_tickets_cstm fstc ON fstc.id_c = fst.id
+    LEFT OUTER JOIN ref_users owner_user ON owner_user.sugar_id = fst.assigned_user_id
+    LEFT OUTER JOIN ref_users creator ON creator.sugar_id = fst.created_by
     WHERE fst.deleted = 0
 );
 
 select count(*) NumberOfCases from mig_case;
-select '';
-update mig_case set ContactId = (select contacts_fst_field_service_tickets_1contacts_ida from contacts_fst_field_service_tickets_1_c where contacts_f061etickets_idb = mig_case.External_ID__c and deleted = 0 limit 1) where ContactId is null;
-update mig_case set AccountId = (select fst_field_service_tickets_accountsaccounts_ida from fst_field_service_tickets_accounts_c where fst_field_service_tickets_accountsfst_field_service_tickets_idb = mig_case.External_ID__c and deleted = 0 limit 1) where AccountId is null;
-select '';
+
+update mig_case set ContactId = (select contacts_fst_field_service_tickets_1contacts_ida from hunter.contacts_fst_field_service_tickets_1_c where contacts_f061etickets_idb = mig_case.External_ID__c and deleted = 0 limit 1) where ContactId is null;
+update mig_case set AccountId = (select fst_field_service_tickets_accountsaccounts_ida from hunter.fst_field_service_tickets_accounts_c where fst_field_service_tickets_accountsfst_field_service_tickets_idb = mig_case.External_ID__c and deleted = 0 limit 1) where AccountId is null;
+
 select count(*) CasesWithContactId from mig_case where ContactId is not null;
-select '';
 select count(*) CasesWithAccountId from mig_case where AccountId is not null;
